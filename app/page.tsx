8292f7f4e4b6dd952c743b237bd263526380f8cf
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2024-09-18 21:07:52 +0500

    grid section

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { FaHome } from "react-icons/fa";
+import Hero from "./Components/Hero";
+import { FloatingNav } from "./Components/Ui/FloatinigNav";
+import Grid from "./Components/Grid";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome   /> }]}
+        />
+        <Hero />
+        <Grid/>
+      </div>
+    </main>
+  );
+}
